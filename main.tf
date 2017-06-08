8eaa6573c4d11fb0a6b20c31011a84257a519359
Alan Chalmers <achalmers@aconex.com>
2017-06-08 14:58:55 +1000

    add min ELB capacity to prevent asg destroy hapening before the new one is fully functional

--- main.tf
+++ main.tf
@@ -11,34 +11,35 @@
 resource "aws_launch_configuration" "launch_config" {
   lifecycle { create_before_destroy = true }
 
-  image_id = "${var.ami_id}"
-  instance_type = "${var.instance_type}"
-  iam_instance_profile = "${var.iam_instance_profile}"
-  key_name = "${var.key_name}"
-  security_groups = ["${var.security_groups}"]
-  user_data = "${var.user_data}"
+  image_id              = "${var.ami_id}"
+  instance_type         = "${var.instance_type}"
+  iam_instance_profile  = "${var.iam_instance_profile}"
+  key_name              = "${var.key_name}"
+  security_groups       = ["${var.security_groups}"]
+  user_data             = "${var.user_data}"
 }
 
 resource "aws_autoscaling_group" "main_asg" {
   lifecycle { create_before_destroy = true }
 
-  name = "${var.asg_name} - ${aws_launch_configuration.launch_config.name}"
+  name                      = "${var.asg_name} - ${aws_launch_configuration.launch_config.name}"
 
   # The chosen availability zones *must* match the AZs the VPC subnets are tied to.
-  availability_zones = ["${split(",", var.availability_zones)}"]
-  vpc_zone_identifier = ["${split(",", var.vpc_zone_subnets)}"]
+  availability_zones        = ["${split(",", var.availability_zones)}"]
+  vpc_zone_identifier       = ["${split(",", var.vpc_zone_subnets)}"]
 
   # Uses the ID from the launch config created above
-  launch_configuration = "${aws_launch_configuration.launch_config.name}"
+  launch_configuration      = "${aws_launch_configuration.launch_config.name}"
 
-  max_size = "${var.asg_number_of_instances}"
-  min_size = "${var.asg_minimum_number_of_instances}"
-  desired_capacity = "${var.asg_number_of_instances}"
+  max_size                  = "${var.asg_number_of_instances}"
+  min_size                  = "${var.asg_minimum_number_of_instances}"
+  desired_capacity          = "${var.asg_number_of_instances}"
+  min_elb_capacity          = "${var.asg_number_of_instances}"
 
   health_check_grace_period = "${var.health_check_grace_period}"
-  health_check_type = "${var.health_check_type}"
+  health_check_type         = "${var.health_check_type}"
 
-  load_balancers = ["${split(",", var.load_balancer_names)}"]
+  load_balancers            = ["${split(",", var.load_balancer_names)}"]
   tag {
     key                 = "Name"
     value               = "${var.asg_name} ec2"
